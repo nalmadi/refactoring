04404b6e53c389ee41b66f91d60e408d837d0a78
jmconn22 <59665851+MyNameBeJohn@users.noreply.github.com>
2021-11-18 23:58:45 -0500

    Updated pong game with new refactors
    Removed comments, adjusted the collisions

--- after/class_based_pong_after.py
+++ after/class_based_pong_after.py
@@ -8,10 +8,9 @@ description: simple implementation of the game Pong using python 3 turtles.
 
 import turtle
 
-#TODO: Created the game class 
 class Game:
 
-    def __init__(self, p1_score, p2_score, border_1, border_2, top_border, bottom_border):
+    def __init__(self, p1_score, p2_score, border_1, border_2, top_border, bottom_border, padding):
 
         self.p1_score = p1_score
         self.p2_score = p2_score
@@ -20,8 +19,8 @@ class Game:
         self.top_border = top_border
         self.bottom_border = bottom_border
         self.pen = None
+        self.padding = padding
 
-    #TODO: Added to the game class 
     def make_window(self, window_title, bgcolor, width, height):
         '''this function creates a screen object and returns it'''
 
@@ -41,7 +40,6 @@ class Game:
             self.p2_score += 1
             return True
 
-    #TODO: Made part of the game class 
     def make_pen(self, shape, color, width, length, x_pos, y_pos):
         '''Creates a pen to draw the score board'''
 
@@ -62,7 +60,6 @@ class Game:
         self.pen.write(message, align=align, font=font)
         self.pen.hideturtle()
 
-    #TODO: Moved from to the game class 
     def set_top_bottom_bounds(self, yposition):
         '''Sets the top and bottom borders'''
 
@@ -74,8 +71,7 @@ class Game:
 
 class Paddle:
 
-    #TODO: Added more parameters for color, shape, length and width
-    def __init__(self, x_position, y_position, shape, color, length, width):
+    def __init__(self, x_position, y_position, shape, color, length, width, hitbox):
         ''' initializes a paddle with a position '''
 
         self.x_position = x_position
@@ -84,6 +80,7 @@ class Paddle:
         self.color = color
         self.length = length
         self.width = width
+        self.hitbox= hitbox
 
         self.turt = make_turtle(self.shape, self.color, self.length, self.width, self.x_position, self.y_position)
 
@@ -104,27 +101,14 @@ class Paddle:
         self.turt.sety(y)
         self.y_position = y
 
-    #TODO: Combined the xcor and ycor functions
     def return_position(self):
         '''Returns the position of the turtle'''
 
         return self.x_position, self.y_position
 
 
-    #TODO: Redundant methods removed 
-    # def xcor(self):
-    #     ''' returns turtle x_cord '''
-    #     return self.turt.xcor()
-
-    
-    # def ycor(self):
-    #     ''' returns turtle y_cord '''
-    #     return self.turt.ycor()
-
-
 class Ball:
 
-    #TODO: Added more parameters to the ball, also made some names better, got rid of comments since the names were improved
     def __init__(self, shape, color, length, width, x_cordinate, y_cordinate, x_speed = 0.0925, y_speed = 0.0925):
         ''' intializes a ball with default direction and position '''
 
@@ -145,20 +129,9 @@ class Ball:
         self.y_position = self.turt.ycor()
 
 
-    #TODO: Combined these tow functions in one position function
     def return_position(self):
         '''Returns the turtle postion'''
         return self.x_position, self.y_position
-    
-    #TODO: Replaced with return_postion function 
-    # def xcor(self):
-    #     ''' returns turtle x_cord '''
-    #     return self.turt.xcor()
-
-    
-    # def ycor(self):
-    #     ''' returns turtle y_cord '''
-    #     return self.turt.ycor()
 
     def goto(self, x_pos, y_pos):
         ''' moves ball to new x, y positions '''
@@ -176,7 +149,6 @@ class Ball:
         self.turt.sety(y_cor)
         self.y_position = y_cor
 
-# TODO: Left alone 
 def make_turtle(shape, color, stretch_width, stretch_length, x_pos, y_pos):
     ''' creates a turtle and sets initial position '''
 
@@ -192,87 +164,59 @@ def make_turtle(shape, color, stretch_width, stretch_length, x_pos, y_pos):
 
 def main():
     ''' the main function where the game events take place '''
-    game = Game(0, 0, 350, -350, 290, -290)
+    game = Game(0, 0, 350, -350, 290, -290, 10)
 
-    #TODO: Made window part of the game class 
     window = game.make_window("Pong - A CS151 Reproduction!", "black", 800, 600)
 
-    #TODO: made score part of the game class
-    # # Score
-    # score_player1 = 0
-    # score_player2 = 0
+    paddle_1 = Paddle(-350, 0, 'square','white', 5, 1, 50)
+    paddle_2 = Paddle(350, 0, 'square','white', 5, 1, 50)
 
-    # paddels
-    paddle_1 = Paddle(-350, 0, 'square','white', 5, 1)
-    paddle_2 = Paddle(350, 0, 'square','white', 5, 1)
-
-    # ball
     ball = Ball("circle", 'white', 2, 2, 0, 0)
 
     game.make_pen("square", "white", 1, 1, 0, 260)
-    game.create_Scoreboard("Player A: 0  Player B: 0", "center", ("Courier", 24, "normal"))
-
-    #TODO: Made this part of the game class
-    # # Pen
-    # pen = make_turtle("square", "white", 1, 1, 0, 260)
-    # pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
-    # pen.hideturtle()    
+    game.create_Scoreboard("Player A: 0  Player B: 0", "center", ("Courier", 24, "normal"))  
     
     # Keyboard bindings
-    window.listen() #Listen for keyboard input
-    window.onkeypress(paddle_1.up, "w") #when you press w run paddle_a_up
+    window.listen() 
+    window.onkeypress(paddle_1.up, "w") 
     window.onkeypress(paddle_1.down, "s")
     window.onkeypress(paddle_2.up, "Up")
     window.onkeypress(paddle_2.down, "Down")
 
-    # Main game loop
     while True:
-        window.update() #This is the update to offset the wn.tracer(0)
+        window.update()
 
         ball.move()
 
         # Border checking    
-        #TODO: Left and right, in update_scoreboard method
         if game.set_game_score(ball.x_position):
             game.update_Scoreboard("Player A: "+ str(game.p1_score) + "  Player B: "+ str(game.p2_score), align="center", font=("Courier", 24, "normal"))
-            #Added to game class
-            # score_player1 += 1
-            # pen.clear()
-            # pen.write("Player A: "+ str(score_player1) + "  Player B: "+ str(score_player2), align="center", font=("Courier", 24, "normal"))
             ball.goto(0, 0)
             ball.ball_x_speed *= -1
         
-        #TODO: Used Top and Bottom function (was not being used before)
         if game.set_top_bottom_bounds(ball.y_position) == 1:
-            ball.sety(290)
+            print(ball.x_position - game.padding)
+            ball.sety(game.top_border)
             ball.ball_y_speed *= -1
+
         elif game.set_top_bottom_bounds(ball.y_position) == 0:
-            ball.sety(-290)
+            ball.sety(game.bottom_border)
             ball.ball_y_speed *= -1
 
-
-        #TODO: Included in game.update_Scoreboard
-        # elif game.set_game_score(ball.x_position):
-        #     #Added to game class 
-        #     # score_player2 += 1
-        #     # pen.clear()
-        #     # pen.write("Player A: "+ str(score_player1) + "  Player B: "+ str(score_player2), align="center", font=("Courier", 24, "normal"))
-        #     ball.goto(0, 0)
-        #     ball.ball_x_speed *= -1
-
-        # TODO: Paddle and ball collisions (left alone)
-        if ball.x_position < -340 and ball.x_position > -350 and ball.y_position < paddle_1.y_position + 50 and ball.y_position > paddle_1.y_position - 50:
-            ball.setx(-340)
+        if ball.x_position < game.border_2 + game.padding and ball.x_position > game.border_2 and ball.y_position < paddle_1.y_position + paddle_1.hitbox and ball.y_position > paddle_1.y_position - paddle_1.hitbox:
+            ball.setx(game.border_2 + game.padding)
             ball.ball_x_speed *= -1.5
         
-        elif ball.x_position > 340 and ball.x_position < 350 and ball.y_position < paddle_2.y_position + 50 and ball.y_position > paddle_2.y_position - 50:
-            ball.setx(340)
+        elif ball.x_position > game.border_1 - game.padding and ball.x_position < game.border_1 and ball.y_position < paddle_2.y_position + paddle_2.hitbox and ball.y_position > paddle_2.y_position - paddle_2.hitbox:
+            ball.setx(game.border_1 - game.padding)
             ball.ball_x_speed *= -1.5
-
-
-
-
 
 
 if __name__ == "__main__":
 	main()
+
+
+
+
+
+
