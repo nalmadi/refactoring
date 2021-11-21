9216624b51570d3ce4afc850d683d4698849382a
Erik Cohen <ejcohe22@colby.edu>
2021-11-21 15:56:34 -0500

    pong after

--- after/class_based_pong_after.py
+++ after/class_based_pong_after.py
@@ -10,39 +10,32 @@ from graphics import *
 
 class Paddle:
     # implements a Pong game paddle
+    dy = 20
 
     def __init__(self, x_position, y_position):
         ''' initializes a paddle with a position '''
-
         self.x_position = x_position
         self.y_position = y_position
-
-        self.turt = make_turtle("square", "white", 5, 1, self.x_position, self.y_position)
-
+        self.paddle = TurtleGraphics()
+        self.paddle.teleport(x_position, y_position)
+        self.paddle.turtle_style(shape = "square", stretch_width = 5)
+        #self.paddle.get_turtle().hideturtle()
 
     def up(self):
-        y = self.turt.ycor()
-        y += 20
-        self.turt.sety(y)
-        self.y_position = y
-
+        current_y = self.get_paddle_ycor()
+        new_y = current_y + self.dy
+        self.paddle.teleport(self.x_position, new_y)
+        self.y_position = new_y
 
     def down(self):
-        y = self.turt.ycor() #Get the current y coordinate
-        y -= 20             #add 20px could also be y=y+20
-        self.turt.sety(y)    #move the paddle to the new y position
-        self.y_position = y
+        current_y = self.get_paddle_ycor()
+        new_y = current_y - self.dy
+        self.paddle.teleport(self.x_position, new_y)
+        self.y_position = new_y
 
-
-    def xcor(self):
-        ''' returns turtle x_cord '''
-        return self.turt.xcor()
-
-    
-    def ycor(self):
+    def get_paddle_ycor(self):
         ''' returns turtle y_cord '''
-        return self.turt.ycor()
-
+        return self.paddle.get_turtle().ycor()
 
 
 class Ball:
@@ -50,118 +43,157 @@ class Ball:
 
     def __init__(self):
         ''' intializes a ball with default direction and position '''
-
-        self.turt = make_turtle("square", "white", 1, 1, 0, 0)
-        self.ball_dx = 0.0925 #speed in x direction
+        self.ball_dx = 0.6 #speed in x direction
         self.ball_dy = 0.0925 #speed in y direction
         self.x_position = 0
         self.y_position = 0
+        self.ball = TurtleGraphics()
+        self.turtle_ball = self.ball.get_turtle()
+        self.ball.teleport(self.x_position, self.y_position)
+        self.ball.turtle_style(shape = "square")
+
+    def get_ball_xcor(self):
+        return self.ball.get_turtle().xcor()
+
+    def get_ball_ycor(self):
+        return self.ball.get_turtle().ycor()
 
 
     def move(self):
         ''' moves the ball in x and y directions '''
 
-        # Move the ball
-        self.turt.setx(self.turt.xcor() + self.ball_dx)
-        self.turt.sety(self.turt.ycor() + self.ball_dy)
+        new_x_cor = self.get_ball_xcor() + self.ball_dx
+        new_y_cor = self.get_ball_ycor() + self.ball_dy
 
-        self.x_position = self.turt.xcor()
-        self.y_position = self.turt.ycor()
+        # Move the ball
+        self.ball.teleport(new_x_cor,new_y_cor)
+
+        self.x_position = new_x_cor
+        self.y_position = new_y_cor
 
         # Top and bottom
-        if self.turt.ycor() > 290:
-            self.turt.sety(290)
+        self.bounce_off_window()
+
+    def set_ycor(self, new_y):
+        '''set turtle y_cor helper function for bounce_off_window'''
+        return self.ball.get_turtle().sety(new_y)
+
+    def bounce_off_window(self):
+        ''' Checks if ball is out of bounds and "bounces" the ball off the window'''
+        y_pos = self.get_ball_ycor()
+        if y_pos > 290:
+            self.sety(290)
             self.ball_dy *= -1
 
-        elif self.turt.ycor() < -290:
-            self.turt.sety(-290)
+        elif y_pos < -290:
+            self.sety(-290)
             self.ball_dy *= -1
-
-    
-    def xcor(self):
-        ''' returns turtle x_cord '''
-        return self.turt.xcor()
-
-    
-    def ycor(self):
-        ''' returns turtle y_cord '''
-        return self.turt.ycor()
-
 
     def goto(self, x_pos, y_pos):
         ''' moves ball to new x, y positions '''
-        self.turt.goto(x_pos, y_pos)
+        self.ball.teleport(x_pos, y_pos)
         self.x_position = x_pos
         self.y_position = y_pos
 
 
-    def setx(self, x_cor):
-        ''' sets the ball x position '''
-        self.turt.setx(x_cor)
-        self.x_position = x_cor
+class PongWindow:
+    def __init__(self, title):
+        self.turt = TurtleGraphics()
+        self.window = self.turt.make_window(title)
+        # display words on window
+        self.turt.teleport(0, 260)
+        self.pen = self.turt.get_turtle()
+        self.pen.hideturtle()
 
+    def display_score(self, score):
+        self.pen.clear()
+        font = ("Courier", 24, "normal")
+        self.pen.write(score, align="center", font = font)
+
+    def get_window(self):
+        return self.window
+
+
+class Pong:
+    # implements a Pong game
+
+    def __init__(self):
+        ''' intializes a game window, players, and the game objects '''
+        self.pong = PongWindow("Pong - A CS151 Reproduction!")
+        self.window = self.pong.get_window()
+        self.initialize_game()
+
+    def initialize_game(self):
+        self.score_player1 = 0
+        self.score_player2 = 0
+        self.display_score()
+        self.paddle_1 = Paddle(-350, 0)
+        self.paddle_2 = Paddle(350, 0)
+        self.ball = Ball()
+        self.set_key_bindings()
+
+    def set_key_bindings(self):
+        self.window.listen() #Listen for keyboard input
+        self.window.onkeypress(self.paddle_1.up, "w") #when you press w run paddle_a_up
+        self.window.onkeypress(self.paddle_1.down, "s")
+        self.window.onkeypress(self.paddle_2.up, "Up")
+        self.window.onkeypress(self.paddle_2.down, "Down")
+
+    def display_score(self):
+        player1Score = str(self.score_player1)
+        player2Score = str(self.score_player2)
+        self.pong.display_score(f"Player A: {player1Score}  Player B: {player2Score}")
+
+    
+    def point_scored_left(self):
+        self.score_player1 += 1
+        self.display_score
+        self.ball.goto(0, 0)
+        self.ball.ball_dx *= 1
+
+    def point_scored_right(self):
+        self.score_player2 += 1
+        self.display_score
+        self.ball.goto(0, 0)
+        self.ball.ball_dx *= -1
+
+    def check_point_scored(self):
+        left_bound = -350
+        right_bound = 350
+        ball_x_pos = self.ball.get_ball_xcor()
+        if ball_x_pos > right_bound:
+            self.point_scored_left()
+
+        elif ball_x_pos < left_bound:
+            self.point_scored_right()
+
+    def collisionLeftPaddle(self):
+        ballPaddle_x_intercept = self.ball.xcor() < -340 and self.ball.xcor() > -350
+        ballPaddle_y_intercept = self.ball.ycor() < self.paddle_1.ycor() + 50 and self.ball.ycor() > self.paddle_1.ycor() - 50
+        if ballPaddle_x_intercept and ballPaddle_y_intercept:
+            self.ball.setx(-340)
+            self.ball.ball_dx *= -1.5
+
+    def collisionRightPaddle(self):
+        ballPaddle_x_intercept = self.ball.xcor() > 340 and self.ball.xcor() < 350
+        ballPaddle_y_intercept = self.ball.ycor() < self.ball.ycor() < self.paddle_2.ycor() + 50 and self.ball.ycor() > self.paddle_2.ycor() - 50
+        if ballPaddle_x_intercept and ballPaddle_y_intercept:
+            self.ball.setx(340)
+            self.ball.ball_dx *= -1.5
+
+    def collisions(self):
+        self.CollisionLeftPaddle()
+        self.CollisionRightPaddle()
+
+    def run(self):
+        while True:
+            self.window.update() #This is the update to offset the wn.tracer(0)
+            self.ball.move()
+            self.check_point_scored()
 
 def main():
-    ''' the main function where the game events take place '''
-
-    window = make_window("Pong - A CS151 Reproduction!", "black", 800, 600)
-
-    # Score
-    score_player1 = 0
-    score_player2 = 0
-
-    # paddels
-    paddle_1 = Paddle(-350, 0)
-    paddle_2 = Paddle(350, 0)
-
-    # ball
-    ball = Ball()
-
-    # Pen
-    pen = make_turtle("square", "white", 1, 1, 0, 260)
-    pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
-    pen.hideturtle()
-
-    # Keyboard bindings
-    window.listen() #Listen for keyboard input
-    window.onkeypress(paddle_1.up, "w") #when you press w run paddle_a_up
-    window.onkeypress(paddle_1.down, "s")
-    window.onkeypress(paddle_2.up, "Up")
-    window.onkeypress(paddle_2.down, "Down")
-
-    # Main game loop
-    while True:
-        window.update() #This is the update to offset the wn.tracer(0)
-
-        ball.move()
-
-        # Border checking    
-        # Left and right
-        if ball.xcor() > 350:
-            score_player1 += 1
-            pen.clear()
-            pen.write("Player A: "+ str(score_player1) + "  Player B: "+ str(score_player2), align="center", font=("Courier", 24, "normal"))
-            ball.goto(0, 0)
-            ball.ball_dx *= -1
-
-        elif ball.xcor() < -350:
-            score_player2 += 1
-            pen.clear()
-            pen.write("Player A: "+ str(score_player1) + "  Player B: "+ str(score_player2), align="center", font=("Courier", 24, "normal"))
-            ball.goto(0, 0)
-            ball.ball_dx *= -1
-
-        # Paddle and ball collisions
-        if ball.xcor() < -340 and ball.xcor() > -350 and ball.ycor() < paddle_1.ycor() + 50 and ball.ycor() > paddle_1.ycor() - 50:
-            ball.setx(-340)
-            ball.ball_dx *= -1.5
-        
-        elif ball.xcor() > 340 and ball.xcor() < 350 and ball.ycor() < paddle_2.ycor() + 50 and ball.ycor() > paddle_2.ycor() - 50:
-            ball.setx(340)
-            ball.ball_dx *= -1.5
-
-
-
+    pong = Pong()
+    pong.run()
 
 if __name__ == "__main__":
-	main()
+    main()
--- after/graphics.py
+++ after/graphics.py
@@ -8,25 +8,29 @@ description: simple implementation of graphics with turtle
 
 import turtle
 
-def make_window(window_title, bgcolor, width, height):
-    '''this function creates a screen object and returns it'''
+class TurtleGraphics:
+    def __init__(self):  
+        self.turt = turtle.Turtle()
+        self.turt.speed(0) # Speed of animation, 0 is max
+    
+    def turtle_style(self, shape = 'classic', color = "white", stretch_width = 1, stretch_length = 1):
+        self.turt.shape(shape) # square defualt is 20,20
+        self.turt.color(color)
+        self.turt.shapesize(stretch_width, stretch_length) 
 
-    window = turtle.getscreen() #Set the window size
-    window.title(window_title)
-    window.bgcolor(bgcolor)
-    window.setup(width, height)
-    window.tracer(0) #turns off screen updates for the window Speeds up the game
-    return window
+    def get_turtle(self):
+        return self.turt
 
+    def teleport(self, x_pos, y_pos):
+        self.turt.up()
+        self.turt.goto(x_pos, y_pos)
+        self.turt.down()
 
-def make_turtle(shape, color, stretch_width, stretch_length, x_pos, y_pos):
-    ''' creates a turtle and sets initial position '''
-
-    turt = turtle.Turtle()
-    turt.speed(0) # Speed of animation, 0 is max
-    turt.shape(shape) # square defualt is 20,20
-    turt.color(color)
-    turt.shapesize(stretch_width, stretch_length) 
-    turt.penup()
-    turt.goto(x_pos, y_pos) #Start position
-    return turt
+    def make_window(self, window_title, bgcolor = "black", width = 800, height = 600):
+        '''this function creates a screen object and returns it'''
+        window = self.turt.getscreen() #Set the window size
+        window.title(window_title)
+        window.bgcolor(bgcolor)
+        window.setup(width, height)
+        window.tracer(0) #turns off screen updates for the window Speeds up the game
+        return window
